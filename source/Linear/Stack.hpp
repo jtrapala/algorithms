2ded2379b6b1aea45804318801b05e1716356fd9
jtrapala <jane_trapala@my.uri.edu>
2019-06-21 16:02:50 -0400

    First try at stack class

--- source/Linear/Stack.hpp
+++ source/Linear/Stack.hpp
@@ -1,42 +1,60 @@
 #pragma once
-
+#include "LinkedList.hpp"
 /** Stack
  * A container class that stores data using a LIFO
  * (last in, first out) scheme.
  */
 class Stack {
  private:
+ LinkedList list;
+ unsigned int len;
+
  public:
-  Stack();
-  ~Stack();
+  Stack(){
+
+  }
+  ~Stack(){
+  }
 
   /** size()
    * Returns the size of the stack.
    */
-  unsigned int size();
+  unsigned int size(){
+    return list.length();
+  }
 
   /** push(int data)
    * Pushes a node containing the given data to the top of the stack.
    */
-  void push(int data);
+  void push(int data){
+    return list.push_front(data);
+  }
 
   /** pop()
    * Removes and returns the data at the top of the stack.
    */
-  int pop();
+  int pop(){
+    return list.pop_front();
+  }
 
   /** top()
    * Returns the data at the top of the stack without changing the stack.
    */
-  int top();
+  int top(){
+    return list.at(0);
+  }
 
   /** empty()
    * Returns whether or not the stack is empty.
    */
-  bool empty();
+  bool empty(){
+    return (list.length() == 0)?true:false;
+  }
 
   /** clear()
    * Clears the entire stack.
    */
-  void clear();
+  void clear(){
+    list.clear();
+  }
 };
