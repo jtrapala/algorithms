0c0bf85c5cd4eb3b205b1800d505905371f184de
jtrapala <jane_trapala@my.uri.edu>
2019-06-25 22:19:17 -0400

    Stack array implementation passes

--- source/Extra_c/stack.hpp
+++ source/Extra_c/stack.hpp
@@ -1,18 +1,20 @@
 #pragma once
-
+#include <iostream>
 /** Stack
  * A container class that stores data using a LIFO
  * (last in, first out) scheme.
  */
 class Stack {
  private:
-    int n = 10; //max_length
+    int n = 14; //max_length
     int *arr; //At most 10 slots in queue
+    unsigned int len=0;
     int tp;
  public:
   Stack(){
       arr = new int[n];
       tp=-1;
+      len=0;
   }
   ~Stack(){
       delete[] arr;
@@ -22,14 +24,20 @@ class Stack {
    * Returns the size of the stack.
    */
   unsigned int size(){
-      return ((unsigned int)n-(unsigned int)tp);
+      if(tp < 0) return 0;
+      else return len;
   }
 
   /** push(int data)
    * Pushes a node containing the given data to the top of the stack.
    */
   void push(int data){
-    (tp >= (n-1))? throw "Top will be out of bounds of stack array": arr[++tp]=data;
+    if(tp >= (n-1)) throw "Top will be out of bounds of stack array";
+    else{
+        arr[++tp]=data;
+        len++;
+    } 
+
   }
 
   /** pop()
@@ -38,8 +46,8 @@ class Stack {
   int pop(){
       if(tp < 0) throw "Can't pop anything, stack is empty";
       else {
-          return arr[tp];
-          tp--;
+          return arr[tp--];
+          len--;
       } 
 
   }
@@ -56,13 +64,21 @@ class Stack {
    * Returns whether or not the stack is empty.
    */
   bool empty(){
-    return(tp < 0);
+    return (tp == -1 )?true:false;
   }
 
   /** clear()
    * Clears the entire stack.
    */
   void clear(){
-      while(tp >-1) tp--;
+      while(tp >-1) {
+          tp--;
+          len--;
+      }
+  }
+  
+  void prt(){
+      for(int i=0;i<=tp;++i) std::cout << arr[i] << ", ";
+      std::cout << std::endl;
   }
 };
